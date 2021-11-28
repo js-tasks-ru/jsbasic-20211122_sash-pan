e205b7ea5872bd0a7fd7c9ffe2bc5715a87f2a97
Aleksandr Pankratov <sash-pan@mail.ru>
2021-11-28 19:01:48 +0300

    Решил еще 4 задачи

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,15 @@
 function checkSpam(str) {
   // ваш код...
+  str = str.toLowerCase();
+    let newStr1;
+    let newStr2;
+    newStr1=str.indexOf("1xbet");
+    newStr2=str.indexOf("xxx");
+    if(newStr1 == -1 && newStr2 == -1)
+    {
+      return(false);
+    }
+    else{
+      return(true);
+    }
 }
