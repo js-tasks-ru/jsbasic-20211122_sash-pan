fceb5c2fe34666ae70e24531b3fb1592cf5bd23c
Aleksandr Pankratov <sash-pan@mail.ru>
2021-12-05 23:23:34 +0300

    сделал задачи 2ого занятия

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
   // ваш код...
+  let sum2 = 0;
+  for (let key in salaries) {
+    if(typeof salaries[key] == 'number' && salaries[key] != Infinity && salaries[key] != -Infinity && !isNaN(salaries[key])){
+      sum2 += salaries[key];
+    };
+
+  }
+  return(sum2);
 }
